6c46c2d0960f637c98ab90132ad77d3dd1b7485d
Francisco <francisco.s.student@lyonscommunityschool.org>
2017-04-26 14:19:39 +0000

    Fran without Isaiah

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,33 +5,42 @@
 
 function divide(x,y) {
 	//Write your code below this line
-
+var divide = x/y;
+return divide;
 }
 
 function multiply(x,y) {
-
+var product= x*y;
+return product;
 }
 
 function subtract(x,y) {
-
+var subtract= x-y;
+return subtract;
 }
 
 function add(x,y) {
-
+var sum= x+y;
+return sum;
 }
 
 function power(x,y) {
-
+var power = Math.pow(x,y);
+return power;
 }
 
 function square_root(x) {
-
+var total= Math.sqrt(x);
+return total;
 }
 
 function logBase10(x) {
-
+var total = Math.log(x);
+return total;
 }
 
 function generateRandomNumber() {
+    var total =Math.floor(Math.random() *20);
+    return total;
 
 }
